9f0fa76c955e5307417fbc95295a0bb499b81078
Cesar Perez <kaesarbcn@hotmail.com>
2014-02-22 00:26:31 +0100

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Cesar Perez Laguna on 22/02/14.
+//  Copyright (c) 2014 Cesar Perez Laguna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Cesar Perez Laguna on 22/02/14.
+//  Copyright (c) 2014 Cesar Perez Laguna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
